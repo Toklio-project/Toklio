04546862699aa4faae0e815e3c41e19b5da0f56c
Romain Bergery <contact@evaveo.com>
2018-05-04 16:19:30 +0200

    Commit

--- src/debug_utilities/object_sizes.cpp
+++ src/debug_utilities/object_sizes.cpp
@@ -104,12 +104,12 @@ int main(int argc, char* argv[])
   SL(tools::wallet2::unsigned_tx_set);
   SL(tools::wallet2::signed_tx_set);
 
-  SL(Monero::WalletImpl);
-  SL(Monero::AddressBookRow);
-  SL(Monero::TransactionInfoImpl);
-  SL(Monero::TransactionHistoryImpl);
-  SL(Monero::PendingTransactionImpl);
-  SL(Monero::UnsignedTransactionImpl);
+  SL(Toklio::WalletImpl);
+  SL(Toklio::AddressBookRow);
+  SL(Toklio::TransactionInfoImpl);
+  SL(Toklio::TransactionHistoryImpl);
+  SL(Toklio::PendingTransactionImpl);
+  SL(Toklio::UnsignedTransactionImpl);
 
   return 0;
 }
